2fd102bb58a7b75b8587e8103026441fe9eeae88
MINEDUCYT <MINEDUCYT@ANGEL>
2024-11-05 11:35:22 -0600

    Se a agregado la clase cliente

---
+++ Clases/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clave3_Grupo4.Clases
+{
+    class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DUI { get; set; }
+        public string TipoProducto { get; set; }
+        public decimal BilleteraVirtual { get; set; }
+    }
+}
