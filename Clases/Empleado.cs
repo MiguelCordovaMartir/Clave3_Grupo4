7356615fd8e7613e986077180c0fc26b6401e9fc
MINEDUCYT <MINEDUCYT@ANGEL>
2024-11-05 11:37:19 -0600

    Se a agregado la clase Empleado

---
+++ Clases/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clave3_Grupo4.Clases
+{
+    class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string DUI { get; set; }
+        public string Rol { get; set; }
+    }
+}
